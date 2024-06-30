2178a6c8ea24dda5dca79e0daf191603fb71d50d
Anto <bueno@uoc.edu>
2024-06-30 17:35:46 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://blog-antoniobuen0s-projects.vercel.app/", // replace this with your deployed domain
+  author: "Antonio Bueno",
+  desc: "Espacio personal de ideas y curiosidades.",
+  title: "Un blog de Anto",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
@@ -12,8 +12,8 @@ export const SITE: Site = {
 };
 
 export const LOCALE = {
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+  lang: "es", // html lang code. Set this empty and default will be "en"
+  langTag: ["es-ES"], // BCP 47 Language Tags. Set this empty [] to use the environment default
 } as const;
 
 export const LOGO_IMAGE = {
