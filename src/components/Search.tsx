962a35ff1532e23aceb46cb0fc2e7a9edd573853
Anto <bueno@uoc.edu>
2024-06-30 13:59:18 +0200

    Traducciones y personalización
    Algunos textos

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -87,7 +87,7 @@ export default function SearchBar({ searchList }: Props) {
         border-opacity-40 bg-skin-fill py-3 pl-10
         pr-3 placeholder:italic placeholder:text-opacity-75 
         focus:border-skin-accent focus:outline-none"
-          placeholder="Search for anything..."
+          placeholder="Escribe un tema o concepto..."
           type="text"
           name="search"
           value={inputVal}
@@ -100,11 +100,10 @@ export default function SearchBar({ searchList }: Props) {
 
       {inputVal.length > 1 && (
         <div className="mt-8">
-          Found {searchResults?.length}
+          ¡ {searchResults?.length}
           {searchResults?.length && searchResults?.length === 1
-            ? " result"
-            : " results"}{" "}
-          for '{inputVal}'
+            ? " resultado salvaje!"
+            : " resultados salvajes!"}{" "}
         </div>
       )}
 
