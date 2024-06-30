10df508ae99cde79cd7d1be7c06c41a92afc1db2
Anto <bueno@uoc.edu>
2024-06-30 17:57:28 +0200

    cambios base

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -30,7 +30,7 @@ export default function Datetime({
       </svg>
       {modDatetime && modDatetime > pubDatetime ? (
         <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
-          Updated:
+          A fecha de:
         </span>
       ) : (
         <span className="sr-only">Published:</span>
